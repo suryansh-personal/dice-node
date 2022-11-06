d1ad00280fde44689e2aa9468a539c556ca2b6a3
Suryansh Srivastava <suryansh.personal1@gmail.com>
2022-11-06 11:35:54 +0530

    My First Node JS Deployment

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+console.log(__dirname)
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
